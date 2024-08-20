0864cf610f02a6baec7c3b2980bcb252b2c901e2
jaltez <jaltez1@gmail.com>
2024-08-21 00:18:17 +0200

    Mockup (II)

--- resources/views/mockup/index.blade.php
+++ resources/views/mockup/index.blade.php
@@ -18,7 +18,7 @@
             <!-- Top menu -->
             <div class="navbar bg-base-100">
                 <div class="flex-none lg:hidden">
-                    <label for="my-drawer" class="btn btn-square btn-ghost">
+                    <label for="my-drawer" class="btn btn-square">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                             class="inline-block w-6 h-6 stroke-current">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -26,15 +26,17 @@
                         </svg>
                     </label>
                 </div>
-                <div class="flex-1">
-                    <label for="my-drawer" class="btn btn-ghost drawer-button">
-                        Sidebar
+                <div class="flex-none">
+                    <label for="my-drawer" class="btn drawer-button">
+                        <x-ri-side-bar-fill class="h-6" />
                     </label>
+                </div>
+                <div class="flex-auto justify-center">
                     <a class="btn btn-ghost normal-case text-xl">Chat App</a>
                 </div>
                 <div class="flex-none">
-                    <button class="btn btn-ghost">
-                        Settings
+                    <button class="btn btn-square">
+                        <x-ri-settings-3-fill class="h-6" />
                     </button>
                 </div>
             </div>
