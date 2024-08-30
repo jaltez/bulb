8b7256cd4206930c0b513bb05d1f924b0b84fc45
jaltez <jaltez1@gmail.com>
2024-08-31 01:15:29 +0200

    Basics (III)

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
+
+    public function chat()
+    {
+        return $this->belongsTo(Chat::class);
+    }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,14 @@
 
 namespace App\Providers;
 
+use App\Models\Chat;
+use App\Models\Message;
+use App\Models\User;
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +27,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // HTTP
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
+        // Dates
+        Date::use(CarbonImmutable::class);
+
+        // Eloquent
+        Model::shouldBeStrict();
+
+        Relation::requireMorphMap();
+
+        Relation::morphMap([
+            'user' => User::class,
+            'chat' => Chat::class,
+            'message' => Message::class,
+        ]);
     }
 }
